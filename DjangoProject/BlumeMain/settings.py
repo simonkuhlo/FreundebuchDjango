dfb6c29976fb28b4b4ffa3a0fc02beadc699fb97
simonkuhlo <siga241@gmail.com>
2026-01-11 16:20:03 +0100

    improve production docker setup

--- DjangoProject/BlumeMain/settings.py
+++ DjangoProject/BlumeMain/settings.py
@@ -124,12 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / "static",
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
