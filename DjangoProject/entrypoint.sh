ca4d807751a807e7ccfa7c41f7eeea8da34b64a6
simonkuhlo <siga241@gmail.com>
2026-02-06 21:34:08 +0100

    docker changes

--- DjangoProject/entrypoint.sh
+++ DjangoProject/entrypoint.sh
@@ -6,8 +6,7 @@ python manage.py migrate --noinput
 # Gunicorn with structured logging to stdout
 exec python -m gunicorn \
   --bind 0.0.0.0:8000 \
-  --workers $(expr $(nproc) \* 2 + 1) \
-  --worker-class uvicorn.workers.UvicornWorker \
+  --workers 3 \
   --log-level info \
   --access-logfile - \
   --error-logfile - \
